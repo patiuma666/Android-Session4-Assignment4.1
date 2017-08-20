8d98df58c033e7567c5b8ca8fb00e4cdb42695ed
patiuma666 <patiuma666@gmail.com>
2017-08-20 20:33:17 +0530

    Add files via upload

---
+++ MyActivity.java
@@ -0,0 +1,16 @@
+package com.example.iis5.loginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+    }
+
+}
